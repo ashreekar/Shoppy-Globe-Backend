d896055dd3d28f5e48ea4744150747aeaf8f1397
ashreekar <ashreekar767@gmail.com>
2025-11-11 08:26:02 +0530

    updated vendor get route or vendors and controller

--- src/routes/vendor.route.js
+++ src/routes/vendor.route.js
@@ -3,9 +3,12 @@ import { verifyJwt } from "../middleware/verifyJWT.js";
 import { loginVendor, registerVendor } from "../controller/vendor.controller.js";
 import { logoutUser } from "../controller/user.controller.js";
 import { verifyLoginUserFields, verifyRegisterUserFields, verifyVendorExists } from "../middleware/verifyUsercontrols.js"
+import { verifyVendor } from "../middleware/verifyVendor.js";
+import { getOrdersForVendor } from "../controller/checkout.controller.js";
 
 const router = Router();
 
+router.get('/orders').get(verifyJwt, verifyVendor, getOrdersForVendor);
 router.route('/register').post(verifyRegisterUserFields, verifyVendorExists, registerVendor)
 router.route('/login').post(verifyLoginUserFields, loginVendor)
 router.route('/logout').post(verifyJwt, logoutUser)
